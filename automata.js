4332ff673d44818973ab7700b1c5fbb7301dc470
David Young <ohnoitsyou7043@gmail.com>
2014-12-30 15:26:42 -0500

    Adding initial framework

--- automata.js
+++ automata.js
@@ -1,16 +1,21 @@
-var app = require('espress')()
-  , server = require('http').Server(app);
-  , q = require('q'),
+var express = require('express')
+  , hbs = require('hbs')
+  , app = express()
+  , server = require('http').Server(app)
+  , q = require('q')
   , config = require('config')
+  , broadway = require('broadway')
   ;
 
 var debug = false;
 
+server.listen(config.get('port'));
+console.log('Listening on ' + config.get('port'));
 
-server.listen(4000);
-
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static('bower_components'));
 
 app.get('/', function(req, res) {
-  res.send('Hello world');
+  res.render('index');
 });
