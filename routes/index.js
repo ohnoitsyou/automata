a07648b953a75978368a5d496a4ceb99ae8291a9
David Young <ohnoitsyou7043@gmail.com>
2015-06-11 20:12:27 -0400

    Working on rendering

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('api', { title: 'Automata' });
+  res.render('api', { title: 'Automata',pluginDir: res.locals.pluginDir});
 });
 
 module.exports = router;
