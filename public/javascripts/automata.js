4a0379f7ae8917b78d2488195111f2905393f4d6
David Young <ohnoitsyou7043@gmail.com>
2015-06-30 00:30:20 -0400

    Making things lineup properly

--- public/javascripts/automata.js
+++ public/javascripts/automata.js
@@ -3,7 +3,11 @@ function renderDisplays() {
   loadedPlugins.forEach(function(plugin) {
     $.ajax("/api/" + plugin + "/render", { method: "get" })
       .done(function(data) {
-        $('#elements').append(data);
+        var rowdiv = document.createElement("div");
+        rowdiv.className = "row";
+        $(rowdiv).append('<div class="col-md-2"><h3>' + plugin + '</h3></div');
+        $(rowdiv).append($(data).addClass("col-md-10, plugin"));
+        $("#elements").append(rowdiv);
       });
   });
 };
