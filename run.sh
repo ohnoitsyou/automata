d9a593fb924540a9f9077ca2653c2f952f1740e2
David Young <ohnoitsyou7043@gmail.com>
2015-07-04 12:32:23 -0400

    Adding weaved to the debug list

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 reset;
-DEBUG=automata,automata2:*,PluginLoader,spark,lights,pirelay,relay ./bin/www
+DEBUG=automata,automata2:*,PluginLoader,spark,lights,pirelay,relay,weaved ./bin/www
 #DEBUG=scratch ./bin/www
 
